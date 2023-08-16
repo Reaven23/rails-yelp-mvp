43417367697776e7248ace090d4184b361b45c48
Adrien Regis <adrien.regis@gmail.com>
2023-08-16 13:21:12 +0200

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+may_bistro = {name: "May Bistro", address: "Gournay sur Marne", category: "chinese"}
+bkk = {name: "bkk", address: "Mitry banks", category: "french"}
+surpriz = {name: "surpriz", address: "Paris 11", category: "belgian"}
+
+[dishoom, pizza_east, may_bistro, bkk, surpriz].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
